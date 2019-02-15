a1cd942fef1696fdc9d11944a25ec193fc15fb1f
mimi4Real <706649059@qq.com>
2019-02-15 12:03:26 +0800

    init project

---
+++ webpack.common.js
@@ -0,0 +1,75 @@
+const webpack = require('webpack')
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+	entry: {
+		index: './src/index.js'
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist/js')
+	},
+	module: {
+		rules: [
+
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+
+				}
+			},
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: ExtractTextPlugin.extract({
+					fallback: "style-loader",
+					use: {
+						loader: 'css-loader',
+					}
+				})
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [{
+						loader: 'css-loader',
+					}, {
+						loader: 'sass-loader',
+					}]
+				})
+			}/*, {
+				test: /\.(png|svg|jpg|gif)$/,
+				exclude: /node_modules/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '../img/[name].[ext]'
+					}
+				}]
+			}*/
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('../css/[name].css'),
+
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		}),
+		getEntryHtml()
+	]
+}
+
+function getEntryHtml() {
+	const entryHtml = {
+		template: 'src/view/index.html'
+	}
+	isProd && (entryHtml.filename = path.resolve(__dirname, './dist/index.html'))
+	return new HtmlWebpackPlugin(entryHtml);
+}
---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		/*host:'localhost',
+		port: 9000,
+		open: true,*/
+		host: '127.0.0.1',
+		port: 8085,
+		publicPath: '/',
+		open: true,
+	},
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('development')
+		})
+	]
+});
